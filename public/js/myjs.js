bc3c1b532d4c273e8183f9e43b81be04d8d258fd
Did-hub <65404163+Did-hub@users.noreply.github.com>
2022-12-16 14:32:50 +0100

    Update myjs.js

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -1,5 +1,5 @@
 function changeSubStringColor(RplArg) {
     var str = document.getElementById("demo").innerHTML; 
-    var res = str.replaceAll(RplArg, 'Move');
+    var res = str.replaceAll(RplArg, "<span style='color:red'>" + RplArg + "</span>");
     document.getElementById("demo").innerHTML = res;
 }  
